a0d6016a796d22f47e2e054a64a77d4f8c7ebbf8
Liza Darwesh <l.darwesh23@gmail.com>
2022-06-02 16:36:51 +0200

    expansion and fix on selection

--- quantum-circuit-optimization/qubit_allocation.py
+++ quantum-circuit-optimization/qubit_allocation.py
@@ -100,21 +100,21 @@ class Allocation:
         """
         Random qubit allocation
         """
-        # self.topology = (random.sample(range(self.qubits), 4))
-        # print(self.topology)
+        self.topology = (random.sample(range(self.qubits), 4))
+        print(self.topology)
 
         """
         Linear sequential qubit allocation
         """
         # self-assigned qubit allocation linearly for testing
-        # [0, 1, 2, 3, 4, 5]
-        for i in range(self.qubits):
-            self.topology.append(i)
+        # # [0, 1, 2, 3, 4, 5]
+        # for i in range(self.qubits):
+        #     self.topology.append(i)
 
         #TODO: get the outdegrees to match for the mapping
         outdegree_graph, outdegree_top = self.weighted_graph()
-        print(f'weighted graph = {outdegree_graph}')
-        print(f'topology = {outdegree_top}')
+        # print(f'weighted graph = {outdegree_graph}')
+        # print(f'topology = {outdegree_top}')
 
         # g = {}
 
