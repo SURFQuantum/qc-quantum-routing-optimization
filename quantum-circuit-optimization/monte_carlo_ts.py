195c9dc521ae5fae199c627e8922c860a07467fa
Liza Darwesh <l.darwesh23@gmail.com>
2022-06-02 15:19:05 +0200

    expansion and fix on selection

--- quantum-circuit-optimization/monte_carlo_ts.py
+++ quantum-circuit-optimization/monte_carlo_ts.py
@@ -2,7 +2,7 @@ import itertools
 from math import log, sqrt, e, inf
 from sre_parse import State
 
-
+import random
 from agent import Agent
 from circuit import Circuit
 from qubit_allocation import Allocation
@@ -17,11 +17,29 @@ def pairwise(iterable):
 class Node:
     def __init__(self):
         self.reward = 1
-        self.N = 1
-        self.n = 0
+        self.parent_visits = 1
+        self.child_visits = 1
         self.children: list[Node] = []
         self.parent = None
+        self.expanded = False
         self.action = None
+        self.ucb = 0
+        self.cnot = None
+
+    def add_child(self, child):
+        self.children.append(child)
+        child.parent = self
+
+    def add_children(self, children):
+        for child in children:
+            self.add_child(child)
+
+    def update_win_value(self, value):
+        self.reward += value
+        self.child_visits += 1
+
+        if self.parent:
+            self.parent.update_win_value(value)
 
 class MCTS:
 
@@ -35,8 +53,10 @@ class MCTS:
         self.N = 0
         self.n = 0
         self.n_qubits = circuit.n_qubits
+        #self.n_qubits = 6
         self.root = Node()
         self.connectivity = allocation.connectivity()
+        self.node_evaluator = lambda child, montecarlo: None
 
     # Node is a circuit with gates, parent node should change for every action, child node is the possible action
     # coming from parent node
@@ -54,14 +74,14 @@ class MCTS:
         """
         Upper Confidence Bound for selecting the best child node
         """
-        ucb = node_i.reward + sqrt(2) * (sqrt(log(node_i.N + e + (10 ** (-6))) / (node_i.n + 10 ** (-1))))
+        ucb = node_i.reward + sqrt(2) * (sqrt(log(node_i.parent_visits + e + (10 ** (-6))) / (node_i.child_visits + 10 ** (-1))))
         return ucb
 
     def swap_schedule(self, i, end_state, gate):
 
         a, b, _ = i
-        end_distance = 100
-        print(i)
+        end_distance = inf
+        print(f'gate is {gate}')
 
         # CNOT-gate
         new_gate = [gate[0], gate[1]]
@@ -73,7 +93,9 @@ class MCTS:
             elif new_gate[x] == b:
                 new_gate[x] = a
         new_gate.append(0)
-        print(f' new CNOT-gate position {new_gate}')
+
+        print(f'new gate is {new_gate}')
+        #print(f' new CNOT-gate position {new_gate}')
 
         # calculate the distance to an operable qubit connectivity location
         for i in self.connectivity:
@@ -98,7 +120,9 @@ class MCTS:
         else:
             # if distance to an operable qubit location is more than 4, then this swap location is not recommended
             reward = -1
-        return end_state, reward, new_gate, i
+
+        print(reward)
+        return end_state, reward, new_gate
 
     def selection(self, gate):
         # receives iteration
@@ -109,54 +133,73 @@ class MCTS:
         circuit = []
         action = self.action
         self.root.action = gate
-        child = Node()
         end_state = False
         timestep = 0
-        N = 6 #number of max iterations
+        N = 3 #number of max iterations
 
         # Iterate through the children until the CNOT is operable
         while not end_state:
             timestep += 1
             for i in action:
+                child = Node()
                 child.action = i
-                end_state, reward, new_gate, act = self.swap_schedule(child.action, end_state, gate)
+                end_state, reward, new_gate = self.swap_schedule(child.action, end_state, gate)
+                child.ucb = self.ucb(child)
+                child.cnot = new_gate
                 if end_state:
-                    self.root.children.append(child)
+                    child.reward += reward
+                    child.child_visits = timestep
+                    self.root.add_child(child)
                     break
                 child.reward = reward
-                child.n = timestep
-                self.root.children.append(child)
+                child.child_visits = timestep
 
-            if not end_state:
-                gate = new_gate
+                self.root.add_child(child)
 
             # Find the best child
             child = self.select_child(self.root)
-            print(f' Best child = {child.action}')
-            circuit.append(child.action)
+            gate = child.cnot
+            self.root = child
 
+            circuit.append(child.action)
             # Not more than 6 iterations for selection
             if timestep == N:
                 break
-            if end_state:
-                gate = new_gate
 
         circuit.append(gate)
         print(f' Circuit is {circuit}')
-        return circuit, reward
+        return circuit, child
 
     # function for the result of the simulation
-    def expand(self):
+    def expand(self, root):
+        end_state = False
+        random_node = Node()
+        random_node.action = root.action
 
-        # while non_terminal(node):
-        #     node = rollout_policy(node)
-        # return result(node)
-        pass
+        while random_node.action == root.action:
+            random_node.action = random.choice(self.action)
+
+        random_node.ucb = self.ucb(random_node)
+        _, reward, new_gate = self.swap_schedule(random_node.action, end_state, random_node.cnot)
+
+        random_node.reward = reward
+        random_node.cnot = new_gate
+
+        return random_node
+
 
     # function for randomly selecting a child node
-    def rollout_policy(self):
+    def rollout_policy(self, root):
         # return pick_random(node.children)
-        pass
+        child = random.choice(root.children)
+        root.children = []
+        root.add_child(child)
+        child_win_value = child.ucb
+
+        if child_win_value != 0:
+            root.update_win_value(child_win_value)
+        else:
+            self.rollout_policy(child)
 
     # function for backpropagation
     def backpropagation(self):
@@ -166,26 +209,37 @@ class MCTS:
         pass
 
     def select_child(self, root):
+        best_children = []
+        best_score = float('-inf')
 
-        while bool(root.children):
-            children = root.children
+        for child in root.children:
+            score = child.ucb
+            print(score)
 
-            best = None
-            best_ucb = inf
-            for child in children:
-                ucb = self.ucb(child)
-                if ucb < best_ucb:
-                    best_ucb = ucb
-                    best = child
-            root = best
-        return root
+            if score > best_score:
+                best_score = score
+                best_children = [child]
+            elif score == best_score:
+                best_children.append(child)
+        select = root.action
+        while select == root.action:
+            child_node = random.choice(best_children)
+            select = child_node.action
+        return child_node
+
+    def simulate(self, expansion_count = 1):
+        for i in range(expansion_count):
+            current_node = self.root
+            while current_node.expanded:
+                current_node = current_node.get_preferred_child(self.root)
+
+            self.expand(current_node)
 
     def mcts(self, gate):
-        circuit, _ = self.selection(gate)
-        self.expand()
+        circuit, child = self.selection(gate)
+        self.expand(child)
         self.backpropagation()
         return circuit
-
 
 c = Circuit(4)
 s = State()
@@ -199,5 +253,5 @@ for i in circ:
         break
 
 m = MCTS(a, c, all)
-t = [0, 3, 0]
+t = [3, 0, 0]
 m.mcts(t)
--- quantum-circuit-optimization/test_circuits/test.qasm
+++ quantum-circuit-optimization/test_circuits/test.qasm
@@ -1,6 +1,6 @@
 OPENQASM 2.0;
 include "qelib1.inc";
-qreg q[4];
+qreg q[6];
 cx q[0],q[1];
 cx q[3],q[2];
 cx q[3],q[0];
